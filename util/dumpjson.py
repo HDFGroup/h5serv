3333f7891343ac9c98c34846993ca879691fcf4a
John Readey <jreadey@hdfgroup.org>
2015-02-05 18:49:19 -0800

    fixed dumpjson util

--- util/dumpjson.py
+++ util/dumpjson.py
@@ -22,228 +22,225 @@ class DumpJson:
     def __init__(self):
         pass
         
-    def dumpAttribute(self, obj_uri, jsonOut):
-        req = self.endpoint + obj_uri 
+    #
+    # Make request to service, convert json response to python dictionary 
+    # and return.
+    #      
+    def makeRequest(self, uri):
+        endpoint = self.endpoint
+        if not endpoint:
+            endpoint = "http://" + self.domain
+        endpoint += ':'
+        endpoint += str(self.port)
+        req = endpoint + uri
+        if self.verbose:
+            print "REQ:", req
+        #print "headers:", self.domain
         headers = {'host': self.domain}
         rsp = requests.get(req, headers=headers)
+        if self.verbose:
+            print "RSP:", rsp.status_code
+            
         if rsp.status_code != 200:
-            print "GET", obj_uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        name = rspJson['name']
-        jsonOut[name] = rspJson
-        return True
+            raise Exception("got bad httpstatus: " + str(rsp.status_code) +
+                " for request: " + uri);
+        #print "got response text:", rsp.text
+        rsp_json = json.loads(rsp.text)
+        return rsp_json
         
+        
+    def dumpAttribute(self, obj_uri):
+        rsp_json = self.makeRequest(obj_uri)
+        attr_json = {}
+        attr_json['name'] = rsp_json['name']
+        attr_json['type'] = rsp_json['type']
+        attr_json['shape'] = rsp_json['shape']
+        attr_json['value'] = rsp_json['value']
+        return attr_json
         
     def dumpAttributes(self, uri, jsonOut):
-        req = self.endpoint + uri 
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET", uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        attributesJson = rspJson['attributes']
+        rsp_json = self.makeRequest(uri)
+        attributes_json = rsp_json['attributes']
         
-        if len(attributesJson) > 0:
-            attrDict = {}
-            jsonOut['attributes'] = attrDict
-            for item in attributesJson:
-                name = item['name']   
-                ok = self.dumpAttribute(uri + '/' + name, attrDict)
-                if not ok:
-                    return False
-        return True
-        
-    def dumpLink(self, obj_uri, jsonOut):
-        req = self.endpoint + obj_uri 
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET", obj_uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        name = rspJson['name']
-        jsonOut[name] = rspJson
-        return True
+        if len(attributes_json) > 0:
+            items = []
+            
+            for attr in attributes_json:
+                name = attr['name']
+                
+                if self.noAttrData:
+                    # just copy what we got from "attributes" request
+                    items.append(attr)
+                else:
+                    # fetch the attribute data
+                    uri_attr_request = uri + "/" + name
+                    item = self.dumpAttribute(uri_attr_request)
+                    items.append(item)
+            jsonOut['attributes'] = items;
         
     def dumpLinks(self, uri, jsonOut):
-        req = self.endpoint + uri 
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET", uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        linksJson = rspJson['links']
+        rsp_json = self.makeRequest(uri)
+        links_json = rsp_json['links']
         
-        if len(linksJson) > 0:
-            linkDict = {}
-            jsonOut['links'] = linkDict
-            for item in linksJson:
-                name = item['name']   
-                ok = self.dumpLink(uri + '/' + name, linkDict)
-                if not ok:
-                    return False
-        return True
-       
+        if len(links_json) > 0:
+            linkDict = []
+            jsonOut['links'] = links_json
         
     def dumpGroup(self, uri, jsonOut):
-        req = self.endpoint + uri
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET /groups/", group_uuid, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        group_uuid = rspJson['id'] 
-        jsonOut[group_uuid] = rspJson 
-        ok = self.dumpLinks(uri + '/links', jsonOut[group_uuid]) 
-        if not ok:
-            return False
-        
-        ok = self.dumpAttributes(uri + '/attributes', jsonOut[group_uuid]) 
-        if not ok:
-            return False
-        return True
-        
+        rsp_json = self.makeRequest(uri)
+        group_uuid = rsp_json['id'] 
+        jsonOut[group_uuid] = {} 
+        self.dumpLinks(uri + '/links', jsonOut[group_uuid]) 
+        self.dumpAttributes(uri + '/attributes', jsonOut[group_uuid]) 
         
     def dumpGroups(self):
-        req = self.endpoint + "/groups" 
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET /groups returned httpstatus:", rsp.status_code
-            return False
+        uri = "/groups" 
+        rsp_json = self.makeRequest(uri)
         jsonOut = {}
         
         self.json['groups'] = jsonOut
         
-        rspJson = json.loads(rsp.text)
-        groupIds = rspJson['groups']
-        groupIds.append(self.json['root'])  # add in root group
-        for group_uuid in groupIds:
+        group_ids = rsp_json['groups']
+        group_ids.append(self.json['root'])  # add in root group
+        for group_uuid in group_ids:
             uri = "/groups/" + group_uuid
-            ok = self.dumpGroup(uri, jsonOut)
-            if not ok:
-                return False
-        return True
+            self.dumpGroup(uri, jsonOut)
         
     def dumpDataset(self, uri, jsonOut):
-        req = self.endpoint + uri
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET ", uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        dset_uuid = rspJson['id'] 
-        jsonOut[dset_uuid] = rspJson 
+        rsp_json = self.makeRequest(uri)
+        dset_uuid = rsp_json['id'] 
+        dset_json = {}
+        dset_json['shape'] = rsp_json['shape']
+        dset_json['type'] = rsp_json['type']
         
-        req = self.endpoint + uri + '/value'
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET ", uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        jsonOut[dset_uuid]['value'] = rspJson['value']
+        # get the data values    
+        rsp_json = self.makeRequest(uri + '/value')
+        
+        if not self.noDsetData:
+            # get the dataset values
+            dset_json['value'] = rsp_json['value']
+        
+        jsonOut[dset_uuid] = dset_json 
          
-        ok = self.dumpAttributes(uri + '/attributes', jsonOut[dset_uuid]) 
-        if not ok:
-            return False
-        return True
+        self.dumpAttributes(uri + '/attributes', jsonOut[dset_uuid]) 
         
     def dumpDatasets(self):
-        req = self.endpoint + "/datasets" 
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET /datasets returned httpstatus:", rsp.status_code
-            return False
+        rsp_json = self.makeRequest("/datasets")
         jsonOut = {}
         
         self.json['datasets'] = jsonOut
         
-        rspJson = json.loads(rsp.text)
-        datasetIds = rspJson['datasets']
+        dataset_ids = rsp_json['datasets']
         
-        for dset_uuid in datasetIds:
+        for dset_uuid in dataset_ids:
             uri = "/datasets/" + dset_uuid
-            ok = self.dumpDataset(uri, jsonOut)
-            if not ok:
-                return False
-        return True
+            self.dumpDataset(uri, jsonOut)
         
     def dumpDatatype(self, uri, jsonOut):
-        req = self.endpoint + uri
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET ", uri, "returned httpstatus:", rsp.status_code
-            return False
-        rspJson = json.loads(rsp.text)
-        dtype_uuid = rspJson['id'] 
-        jsonOut[dtype_uuid] = rspJson 
+        rsp_json = self.makeRequest(uri)
+        
+        dtype_uuid = rsp_json['id']
+        
+        type_json = {}
+        type_json['type'] = rsp_json['type']
+        
+        jsonOut[dtype_uuid] =  type_json
          
-        ok = self.dumpAttributes(uri + '/attributes', jsonOut[dtype_uuid]) 
-        if not ok:
-            return False
-        return True
+        self.dumpAttributes(uri + '/attributes', jsonOut[dtype_uuid]) 
         
     def dumpDatatypes(self):
-        req = self.endpoint + "/datatypes" 
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET /datatypes returned httpstatus:", rsp.status_code
-            return False
+        rsp_json = self.makeRequest("/datatypes")
         jsonOut = {}
         
         self.json['datatypes'] = jsonOut
         
-        rspJson = json.loads(rsp.text)
-        datatypeIds = rspJson['datatypes']
+        datatype_ids = rsp_json['datatypes']
         
-        for dtype_uuid in datatypeIds:
+        for dtype_uuid in datatype_ids:
             uri = "/datatypes/" + dtype_uuid
-            ok = self.dumpDatatype(uri, jsonOut)
-            if not ok:
-                return False
-        return True
+            self.dumpDatatype(uri, jsonOut)
              
     
     def dumpDomain(self):
-        req = self.endpoint + "/"
-        headers = {'host': self.domain}
-        rsp = requests.get(req, headers=headers)
-        if rsp.status_code != 200:
-            print "GET / returned httpstatus:", rsp.status_code
-            return
-        self.json = json.loads(rsp.text)
+        rsp_json = self.makeRequest("/")
         
-        ok = self.dumpGroups()
+        self.json = {}
         
-        if ok:
-            ok = self.dumpDatasets()
+        # save the root uuid
+        self.json['root'] = rsp_json['root']
+        
+        self.dumpGroups()
+        
+        self.dumpDatasets()
             
-        if ok:
-            ok = self.dumpDatatypes()
+        self.dumpDatatypes()
             
-        if ok:
-            print json.dumps(self.json, sort_keys=True, indent=4)
-           
+        print json.dumps(self.json, sort_keys=True, indent=4)
 
+#
+# Print usage and exit
+#
+def printUsage():
+    print "usage: python dumpjson.py [-v] [-D|d] [-endpoint=<server_ip>]  [-port=<port] <domain>"
+    print "  options -v: verbose, print request and response codes from server"
+    print "  options -D: suppress all data output"
+    print "  options -d: suppress data output for datasets (but not attributes)"
+    print "  options -endpoint: specify IP endpoint of server"
+    print "  options -port: port address of server [default 7253]"
+    print " ------------------------------------------------------------------------------"
+    print "  Example - get 'tall' collection from HDF Group server:"
+    print "       python dumpjson.py tall.data.hdfgroup.org"
+    print "  Example - get 'tall' collection from a local server instance "
+    print "        (assuming the server is using port 5000):"
+    print "        python dumpjson.py -endpoint=127.0.0.1 -port=5000 tall.test.hdfgroup.org"
+    sys.exit(); 
+    
+#
+# main
+#
 def main():
-    if len(sys.argv) < 2:
-        print "usage: dumpjson <domain>"
-        sys.exit(); 
+    
+    nargs = len(sys.argv)
         
     dumper = DumpJson()
-    dumper.endpoint = sys.argv[1]  # "http://127.0.0.1:5000"
-    dumper.domain = sys.argv[1]
+    dumper.verbose = False 
+    dumper.endpoint = None
+    dumper.port = 7253
+    dumper.noDsetData = False
+    dumper.noAttrData = False
+    
+    endpoint_option = "-endpoint="
+    port_option = "-port="
+    
+    option_count = 0
+    
+    for arg in sys.argv:
+        if arg.startswith(endpoint_option):
+            endpoint = arg[len(endpoint_option):]
+            if endpoint.startswith("http"):
+                dumper.endpoint = endpoint
+            else:
+                dumper.endpoint = "http://" + endpoint
+            option_count += 1
+        elif arg.startswith(port_option):
+            port = arg[len(port_option):]
+            dumper.port = int(port)
+            option_count += 1
+        elif arg == "-v":
+            dumper.verbose = True
+        elif arg == "-D":
+            dumper.noDsetData = True
+            dumper.noAttrData = True
+        elif arg == "-d":
+            dumper.noDsetData = True
+            
+     
+    if nargs - option_count <= 1:
+        printUsage()
+        
+    dumper.domain = sys.argv[nargs-1]
     dumper.dumpDomain()
     
-     
 
 main()
 
