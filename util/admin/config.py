b996507c462e29bc4c97c06f960f0c6315a50270
jreadey <jreadey@hdfgroup.org>
2016-01-04 12:36:26 -0800

    added config file

---
+++ util/admin/config.py
@@ -0,0 +1,43 @@
+##############################################################################
+# Copyright by The HDF Group.                                                #
+# All rights reserved.                                                       #
+#                                                                            #
+# This file is part of H5Serv (HDF5 REST Server) Service, Libraries and      #
+# Utilities.  The full HDF5 REST Server copyright notice, including          #
+# terms governing use, modification, and redistribution, is contained in     #
+# the file COPYING, which can be found at the root of the source code        #
+# distribution tree.  If you do not have access to this file, you may        #
+# request a copy from help@hdfgroup.org.                                     #
+##############################################################################
+import os
+import sys
+
+cfg = {
+    'server': '127.0.0.1',
+    'port':   5000,
+    'debug':  True,
+    'datapath': '../../data/',
+    'public_dir': ['public', 'test'],
+    'domain':  'hdfgroup.org',
+    'hdf5_ext': '.h5',
+    'toc_name': '.toc.h5',
+    'toc_folder_filter': '[^@]+@[^@]+\.[^@]+',
+    'local_ip': '127.0.0.1',  # used by lcoal_dns.py
+    'password_file': 'passwd.h5'   
+}
+
+
+def get(x):
+    # see if there is a command-line override
+    option = '--'+x+'='
+    for i in range(1, len(sys.argv)):
+        #print i, sys.argv[i]
+        if sys.argv[i].startswith(option):
+            # found an override
+            arg = sys.argv[i]
+            return arg[len(option):]  # return text after option string
+    # see if there are an environment variable override
+    if x.upper() in os.environ:
+        return os.environ[x.upper()]
+    # no command line override, just return the cfg value
+    return cfg[x]
